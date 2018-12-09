13a0b57c2b242edb68e15b10e9f217466efd5a04
Antonis Antoniou <kountouris7@gmail.com>
2018-12-10 01:08:23 +0200

    tables - models - relationships - articles feature store and display

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ Auth::routes();
 Route::get('home', 'HomeController@index')->name('home');
 Route::get('write-article', 'ArticleController@writeArticle')->name('writeArticle');
 Route::post('publish-article/{id}', 'ArticleController@store')->name('publishArticle');
+
 Route::get('show-articles', 'ArticleController@showArticles')->name('showArticles');
 
